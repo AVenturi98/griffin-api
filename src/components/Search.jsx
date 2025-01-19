0937a4f507f7bb596dc3a0e8c54ca96de4559f37
AVenturi98 <lssndrventuri@gmail.com>
2025-01-19 17:07:31 +0100

    ADD icon for clear search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,21 @@
 import { useContext, useState } from "react"
 import GlobalContext from "../context/GlobalContext"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons"
 export default function Search() {
     const { search, setSearch } = useContext(GlobalContext)
 
-    return (
-        <input type="text" placeholder="search person" value={search} onChange={(e) => setSearch(e.target.value)} className="filter" />
+    function clear() {
+        setSearch('')
+    }
 
+    return (
+        <div className="filter">
+            <input type="text" placeholder="search person" value={search} onChange={(e) => setSearch(e.target.value)} className="filter-name " />
+            <button type="reset" id="clear-filter" onClick={clear}>
+                <FontAwesomeIcon icon={faDeleteLeft} id="icon-filter" />
+
+            </button>
+        </div>
     )
 }
